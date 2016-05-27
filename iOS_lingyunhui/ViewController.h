002b8ff1171b69ada70662f56277a7a64c0872d0
stricker <liuchen195@163.com>
2016-05-27 16:56:21 +0800

    Initial Commit

---
+++ iOS_lingyunhui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_lingyunhui
+//
+//  Created by 刘琛 on 16/5/27.
+//  Copyright © 2016年 cyjd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_lingyunhui/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS_lingyunhui
+//
+//  Created by 刘琛 on 16/5/27.
+//  Copyright © 2016年 cyjd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
